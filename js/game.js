a4959646896739f1fc060faf17a233d7abd3dd01
alex <1ziploc2@gmail.com>
2016-09-23 11:42:49 -0600

    made main character

---
+++ js/game.js
@@ -0,0 +1,23 @@
+var mainPlayer = {
+    name: "Player",
+    rank: "Pawn",
+    speed: 10,
+    power: 1,
+
+    messenger = function (message0, message1, message2){
+        alert(message0);
+        alert(message1);
+        alert(message2);
+    }
+}
+
+mainPlayer.name = prompt("Enter character name.");
+
+alert("Welcome " + mainPlayer.name);
+var phrases = [
+    prompt("Please enter some info for me."), 
+    prompt("Enter different info for me."), 
+    prompt("And again.")
+];
+
+mainPlayer.messenger(phrases[0], phrases[1], phrases[2]);
