b6b79e7b9589cdff7874b9d784f314c629c59d7e
alex <1ziploc2@gmail.com>
2016-10-05 11:23:01 -0600

    changed stuff in class?

--- js/game.js
+++ js/game.js
@@ -22,3 +22,14 @@ var phrases = [
 for (var i = 0 ; i < phrases.length ; i++){
     mainPlayer.messenger(phrases[i]);
 }
+
+function PlayGame(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+var firstPrompt = parseInt( prompt("Enter a number between 0 and 10") );
+var secondPrompt = parseInt( prompt("Enter a number between 11 and 20") );
+
+var score = PlayGame(firstPrompt, secondPrompt);
+alert("Your score is " + score + ".");
+alert("Do you want to play again?");
